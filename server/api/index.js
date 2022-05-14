e45742f972c08ac28066085c13ada1a2e5e58f90
laaners <kirbyalessio@yahoo.it>
2022-05-14 18:28:25 +0200

    Add endpoints for DB (eager)

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,6 @@
 import Express from "express"
-import { InitDatabaseConnection, Models } from "./database"
+import { InitDatabaseConnection, Models, db } from "./database"
+import { Model, Op } from "sequelize";
 
 // Init Express
 const app = Express()
@@ -29,7 +30,101 @@ export default app
 		const events = await Models.PointOfInterest.findAll({
 			include: ["events", "itineraries"],
 		})
+		return res.json(events);
+	});
 
-		res.json(events)
-	})
+	app.get("/events", async (req,res) => {
+		const season = req.query.season?.toLowerCase();
+		if(season != undefined && ["spring", "summer", "autumn", "winter"].includes(season)) {
+			const seasonInterval = {
+				"spring": ["03-21","06-20"],
+				"summer": ["06-21","09-22"],
+				"autumn": ["09-23","12-21"]
+			}
+			if(season !== 'winter') {
+				const events = await Models.Event.findAll({
+					where: db.where(
+						db.literal('substring(cast(date as varchar), 6)'),
+						{[Op.between]: seasonInterval[season]}
+					),
+					include: ["pointOfInterest"]
+				});
+				return res.json(events);
+			}
+			else { //winter date between [12-21,12-31] OR  beetween [01-01,03-20]
+				const events = await Models.Event.findAll({
+					where: db.where(
+						db.literal('substring(cast(date as varchar), 6)'),
+						{[Op.or]: {[Op.between]: ['12-21','12-31'], [Op.between]: ['01-01','03-20']}}
+					),
+					include: ["pointOfInterest"]
+				});
+				return res.json(events);
+			}
+		}
+		else {
+			const events = await Models.Event.findAll({
+				include: ["pointOfInterest"]
+			});
+			return res.json(events);
+		}
+	});
+
+	app.get("/events/:id", async (req, res) => {
+        const id = req.params.id;
+        const event = await Models.Event.findOne({
+			where: { id },
+			include: ["pointOfInterest"]		
+		});
+        return res.json(event);
+    });
+
+	app.get("/itineraries", async (req,res) => {
+		const itineraries = await Models.Itinerary.findAll({
+			include: ["pointsOfInterest"]
+		});
+		return res.json(itineraries);
+	});
+
+	app.get("/itineraries/:id", async (req,res) => {
+		const id = req.params.id;
+		const itinerary = await Models.Itinerary.findOne({
+			where: { id },
+			include: ["pointsOfInterest"]
+		});
+		return res.json(itinerary);
+	});
+
+	app.get("/pois", async (req,res) => {
+		const pois = await Models.PointOfInterest.findAll({
+			include: ["events", "itineraries"]
+		});
+		return res.json(pois);
+	});
+
+	app.get("/pois/:id", async (req,res) => {
+		const id = req.params.id;
+		const poi = await Models.PointOfInterest.findOne({
+			where: { id },
+			include: ["events", "itineraries"]
+		});
+		return res.json(poi);
+	});
+
+	app.get("/services", async (req,res) => {
+		const services = await Models.ServiceType.findAll();
+		return res.json(services);
+	});
+
+	app.get("/services/:id", async (req,res) => {
+		const id = req.params.id;
+		const service = await Models.ServiceType.findOne({ where: { id } });
+		return res.json(service);
+	});
+
+	/*
+/api/search?=<query> to search for a query (add a fuzzy search to get more results?)
+	*/
+
+
 })()
