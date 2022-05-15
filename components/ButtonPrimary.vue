131a9e71480a8fd92a38757678673f2483bda1d6
Andrea Franchini <hello@andreafranchini.com>
2022-05-15 18:02:44 +0200

    Rename ButtonItem to ButtonPrimary and fixed a couple of CSS rules

---
+++ components/ButtonPrimary.vue
@@ -0,0 +1,31 @@
+<template>
+	<nuxt-link :to="link" class="button">{{ title }}</nuxt-link>
+</template>
+
+<script>
+export default {
+	name: "ButtonPrimary",
+	props: {
+		title: { required: true, type: String },
+		link: { required: true, type: String },
+	},
+}
+</script>
+
+<style scoped>
+.button {
+	color: var(--color-bg-dark);
+	display: inline-block;
+	padding: 1em;
+	border: 2px solid var(--color-accent-dark);
+	border-radius: var(--border-radius);
+	background-color: var(--color-accent);
+	font-family: var(--font-family-heading);
+	font-size: var(--font-size-button);
+	font-weight: bold;
+	text-decoration: none;
+}
+.button:hover {
+	background-color: var(--color-accent-dark);
+}
+</style>
