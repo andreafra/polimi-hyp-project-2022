2ba4893c23aef9d2f71ba3f829c99ba6e1416a80
Fabio Camerota <fabiocamerota99@gmail.com>
2022-05-19 17:55:08 +0200

    staticPages

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+    <article>
+        <h1>About Us</h1>
+        <h3>History</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quaerat numquam recusandae, assumenda vero sunt at eaque facilis beatae commodi. Quos reiciendis asperiores voluptatibus suscipit facilis tenetur quia recusandae iure.</p>
+        <img src="https://media-cdn.tripadvisor.com/media/photo-s/04/9c/b9/b0/ponte-real-ferdinando.jpg" alt="Ponte Ferdinando"/>
+        <h3>Nature</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quaerat numquam recusandae, assumenda vero sunt at eaque facilis beatae commodi. Quos reiciendis asperiores voluptatibus suscipit facilis tenetur quia recusandae iure.</p>
+        <img src="https://t3.ftcdn.net/jpg/04/22/54/96/360_F_422549672_YBblFRWVwazeHgI8MYtgFlFNKfAHFiTe.jpg" alt="Spiaggia con mare"/>
+        
+    </article>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage',
+  components: {},
+}
+</script> 
---
+++ pages/contacts.vue
@@ -0,0 +1,14 @@
+<template>
+    <article>
+        <h1>Contact Us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quaerat numquam recusandae, assumenda vero sunt at eaque facilis beatae commodi. Quos reiciendis asperiores voluptatibus suscipit facilis tenetur quia recusandae iure.</p>
+        <img src="https://www.ordinemedicilatina.it/wp-content/uploads/2020/10/logo-minturno.png" alt="Ponte Ferdinando"/>
+    </article>
+</template>
+
+<script>
+export default {
+  name: 'ContactsPage',
+  components: {},
+}
+</script> 
