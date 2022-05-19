689bde91f006f6c271bbc01c981605ad53378ee3
laaners <kirbyalessio@yahoo.it>
2022-05-20 00:02:46 +0200

    Use nuxt-link-exact-active

---
+++ pages/events/_id.vue
@@ -0,0 +1,71 @@
+<template>
+	<article>
+		<span class="category">Event</span>
+		<h1>{{ event.name }}</h1>
+		<h-scroll-view>
+			<div
+				v-for="(x, index) of event.images"
+				:key="index"
+				style="height: 300px; background: red"
+			>
+				{{ x }}
+			</div>
+		</h-scroll-view>
+		<p>{{ event.description }}</p>
+		<div class="row">
+			<div class="column">
+				<h2>Admission</h2>
+				<p>{{ event.admission }}</p>
+			</div>
+			<div class="column">
+				<h2>Hosted at</h2>
+				<card :object="getPoI()" />
+			</div>
+		</div>
+	</article>
+</template>
+
+<script>
+import HScrollView from "~/components/HScrollView.vue"
+import Card from "~/components/Card.vue"
+export default {
+	name: "EventPage",
+	components: {
+		HScrollView,
+		Card,
+	},
+	async asyncData({ $axios, params }) {
+		const res = await $axios.$get(`/api/events/${params.id}`)
+		return { event: res }
+	},
+	data() {
+		return { event: {} }
+	},
+
+	methods: {
+		getPoI() {
+			const poi = this.event.pointOfInterest
+			return {
+				title: poi.name,
+				description: poi.description,
+				img: poi.images[0].img,
+				alt: poi.images[0].alt,
+				url: "/pois/" + poi.id,
+			}
+		},
+	},
+}
+</script>
+
+<style scoped>
+.row {
+	display: grid;
+	grid-template-columns: 1fr;
+}
+
+@media only screen and (min-width: 840px) {
+	.row {
+		grid-template-columns: 1fr 1fr;
+	}
+}
+</style>
