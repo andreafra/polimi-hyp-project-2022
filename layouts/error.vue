31e563bd492923d7c8e788ec5879521c88c120ce
Fabio Camerota <fabiocamerota99@gmail.com>
2022-05-29 09:45:26 +0200

    error-page

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<h1 v-if="error.statusCode === 404">Page not found</h1>
+		<h1 v-else>An error occurred</h1>
+		<NuxtLink to="/">Back to Home page</NuxtLink>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "ErrorPage",
+	props: ["error"],
+	// layout: "error", // you can set a custom layout for the error page
+}
+</script>
