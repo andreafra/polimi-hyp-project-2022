bb4594358f63ce227daa402e52bf8ed1db54acdc
CarloReinotti <carlo98.reinotti@gmail.com>
2022-05-31 12:38:04 +0200

    Add services_all page. Add some fields to services entity. Minor fixes to service_types.json

---
+++ pages/services/index.vue
@@ -0,0 +1,47 @@
+<template>
+	<div>
+		<h1>All Services</h1>
+		<p>
+			Here's some of the services that are available right now in the
+			town!
+		</p>
+		<grid-view>
+			<card
+				v-for="service of getGenericService()"
+				:key="service.id"
+				:object="service"
+			/>
+		</grid-view>
+	</div>
+</template>
+
+<script>
+import Card from "~/components/Card.vue"
+import GridView from "~/components/GridView.vue"
+
+export default {
+	name: "AllServicesPage",
+	components: { Card, GridView },
+	async asyncData({ $axios }) {
+		const res = await $axios.$get(`/api/services`)
+
+		return { services: res }
+	},
+	data: () => ({
+		services: [],
+	}),
+
+	methods: {
+		getGenericService() {
+			return this.services.map((service) => ({
+				id: service.id,
+				title: service.name,
+				img: require(`~/assets/images/${service.generic_image}`),
+				alt: service.generic_alt,
+				description: service.description,
+				url: `/services/${service.id}`,
+			}))
+		},
+	},
+}
+</script>
