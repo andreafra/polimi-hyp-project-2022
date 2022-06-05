732cd7cb337554a0ea081edbe0cabba08aa322c2
laaners <kirbyalessio@yahoo.it>
2022-06-05 12:24:22 +0200

    Add natural itinerary; various fixes

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <script>
 import HScrollView from "~/components/HScrollView.vue"
 import Card from "~/components/Card.vue"
-
 export default {
 	name: "IndexPage",
 	components: { HScrollView, Card },
@@ -15,16 +14,21 @@ export default {
 	methods: {
 		getEvents() {
 			return this.events
+				.sort((a, b) => new Date(a.date) - new Date(b.date))
 				.map((event) => ({
 					title: event.name,
-					subtitle: `${event.date}`,
+					subtitle: `${new Date(event.date).toLocaleDateString(
+						"en-GB",
+						{
+							dateStyle: "short",
+						}
+					)}`,
 					img: event.images[0].url,
 					alt: event.images[0].alt,
 					description: event.description,
 					url: `/events/${event.id}`,
 				}))
 				.slice(0, 3)
-				.sort((a, b) => new Date(a.date) - new Date(b.date))
 		},
 	},
 }
@@ -32,13 +36,14 @@ export default {
 
 <template>
 	<div>
-		<img
-			class="welcome-image"
-			src="~/assets/images/home2.jpg?webp"
-			alt="sito archeoleogico"
-		/>
-		<h1>Welcome to Minturno!</h1>
-		<h3>Take a look at our latest events:</h3>
+		<div class="welcome-image">
+			<h1>Welcome to Minturno!</h1>
+			<h2>
+				A place with fascinating monuments,<br />beautiful natural
+				sceneries, delicious cuisine and more!
+			</h2>
+		</div>
+		<h3>Take a look at our upcoming events:</h3>
 		<h-scroll-view>
 			<card
 				v-for="(event, index) of getEvents()"
@@ -53,6 +58,33 @@ export default {
 .welcome-image {
 	margin-top: var(--space-1);
 	width: 100%;
+	height: 30em;
 	border-radius: var(--border-radius);
+
+	background-image: linear-gradient(
+			0deg,
+			var(--color-light),
+			rgba(214, 214, 177, 0)
+		),
+		url("~/assets/images/home2.jpg");
+
+	background-size: cover;
+	background-position: center center;
+	background-repeat: no-repeat;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-end;
+}
+
+h1 {
+	font-size: 3.5em;
+	margin: 0 0 !important;
+}
+
+h2 {
+	color: var(--color-accent-dark);
+	margin: 0 0 !important;
+	margin-bottom: var(--space-2) !important;
 }
 </style>
